7bef58b850632c986c9c848c3e582816307ca8dd
Илья Филиппов <filippov.i.s@gmail.com>
2022-02-27 12:30:45 +0300

    Добавьте файлы проекта.

---
+++ WPF_Program_17/Less17.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Program_17
+{
+    /// <summary>
+    /// Логика взаимодействия для Less17.xaml
+    /// </summary>
+    public partial class Less17 : System.Windows.Controls.UserControl
+    {
+        public Less17()
+        {
+            InitializeComponent();
+        }
+
+        public static DependencyProperty ColorProperty;
+        public static DependencyProperty RedProperty;
+        public static DependencyProperty GreenProperty;
+        public static DependencyProperty BlueProperty;
+        private static readonly RoutedEvent ColorChange;
+
+        static Less17()
+        {
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(Less17),
+                new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(Less17),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(Less17),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(Less17),
+                 new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+            ColorChange = EventManager.RegisterRoutedEvent("ColorChanged", RoutingStrategy.Bubble,
+    typeof(RoutedPropertyChangedEventHandler<Color>), typeof(Less17));
+        }
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+
+        private static void OnColorRGBChanged(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            Less17 colorPicker = (Less17)sender;
+            Color color = colorPicker.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorPicker.Color = color;
+        }
+
+        private static void OnColorChanged(DependencyObject sender,
+      DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+            Less17 less17 = (Less17)sender;
+            less17.Red = newColor.R;
+            less17.Green = newColor.G;
+            less17.Blue = newColor.B;
+
+            RoutedPropertyChangedEventArgs<Color> args = new RoutedPropertyChangedEventArgs<Color>(less17.Color, newColor);
+            args.RoutedEvent = ColorChange;
+            less17.RaiseEvent(args);
+        }
+
+        public event RoutedPropertyChangedEventHandler<Color> ColorChanged
+        {
+            add { AddHandler(ColorChange, value); }
+            remove { RemoveHandler(ColorChange, value); }
+        }
+
+       
+
+    }
+}
